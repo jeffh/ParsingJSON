a460e16933d060dc2bcb086a1a89ce9cc032cf15
Jeff Hui <jeff@jeffhui.net>
2014-10-05 15:53:13 -0700

    Refactored implementation into separate methods.

--- ParsingJSON/PersonParser.m
+++ ParsingJSON/PersonParser.m
@@ -9,23 +9,17 @@ NSInteger kParserErrorCodeBadData = 2;
 
 @implementation PersonParser
 
+#pragma mark - Public
+
 - (Person *)personFromJSONData:(NSData *)jsonData error:(__autoreleasing NSError **)error {
     *error = nil;
 
-    NSError *jsonError = nil;
-    id json = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&jsonError];
-
-    if (jsonError) {
-        *error = [NSError errorWithDomain:kParserErrorDomain
-                                     code:kParserErrorCodeBadData
-                                 userInfo:@{NSUnderlyingErrorKey: jsonError}];
+    id json = [self jsonObjectFromJSONData:jsonData error:error];
+    if (*error) {
         return nil;
     }
-
-    if (json[@"message"]) {
-        *error = [NSError errorWithDomain:kParserErrorDomain
-                                     code:kParserErrorCodeNotFound
-                                 userInfo:@{NSLocalizedDescriptionKey: @"No person was found"}];
+    *error = [self errorMessageFromJSON:json];
+    if (*error) {
         return nil;
     }
 
@@ -40,24 +34,52 @@ NSInteger kParserErrorCodeBadData = 2;
         heightObject = [json[@"height"] description];
     }
     person.height = [[formatter numberFromString:heightObject] unsignedIntegerValue];
+    person.friends = [self friendsWithJSON:json formatter:formatter];
+    return person;
+}
 
+#pragma mark - Private
+
+- (id)jsonObjectFromJSONData:(NSData *)jsonData error:(__autoreleasing NSError **)error {
+    NSError *jsonError = nil;
+    id json = [NSJSONSerialization JSONObjectWithData:jsonData options:0 error:&jsonError];
+    
+    if (jsonError) {
+        *error = [NSError errorWithDomain:kParserErrorDomain
+                                     code:kParserErrorCodeBadData
+                                 userInfo:@{NSUnderlyingErrorKey: jsonError}];
+        return nil;
+    }
+    return json;
+}
+
+- (NSError *)errorMessageFromJSON:(id)json {
+    if (json[@"message"]) {
+        return [NSError errorWithDomain:kParserErrorDomain
+                                   code:kParserErrorCodeNotFound
+                               userInfo:@{NSLocalizedDescriptionKey: @"No person was found"}];
+    }
+    return nil;
+}
+
+- (NSArray *)friendsWithJSON:(id)jsonObject formatter:(NSNumberFormatter *)formatter {
     NSMutableArray *friends = [NSMutableArray array];
-    for (NSDictionary *friendDict in json[@"friends"]) {
+    for (NSDictionary *friendDict in jsonObject[@"friends"]) {
         Person *aFriend = [[Person alloc] init];
         aFriend.identifier = friendDict[@"id"];
         aFriend.name = friendDict[@"name"];
 
-        if ([json[@"height"] isEqual:[NSNull null]]) {
+        NSString *heightObject;
+        if ([jsonObject[@"height"] isEqual:[NSNull null]]) {
             heightObject = @"";
         } else {
             heightObject = [friendDict[@"height"] description];
         }
         aFriend.height = [[formatter numberFromString:heightObject] unsignedIntegerValue];
-
+        
         [friends addObject:aFriend];
     }
-    person.friends = friends;
-    return person;
+    return friends;
 }
 
 @end
